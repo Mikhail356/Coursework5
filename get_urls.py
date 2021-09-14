86a809977a77c07e4fd78140f84b901d84856512
Mikhail356 <michael_gvozdev@mail.ru>
2021-09-14 21:19:44 +0300

    Pretesting

--- get_urls.py
+++ get_urls.py
@@ -1,7 +1,10 @@
 import bs4
+import re
 import json
 import requests
 import sys
+import zlib
+
 from multiprocessing.dummy import Pool, Queue
 from tqdm import tqdm
 """
@@ -9,28 +12,59 @@ Functions to collect urls for websites in mass_media.txt
 """
 
 
-def process_sites(url, api_url):
-    pass
+def process_sites(url, api_url, reg_expr):
+    ans = []
+    r = requests.get(api_url,
+                     params={
+                         'url': url+'*',
+                         'limit': 3,
+                         'output': 'json',
+                         'fl': 'url,filename,offset,length',
+                         'filter': ['=status:200',
+                                    '=language:rus' or '=language:rus,eng',
+                                    ]
+                     })
+    records = [json.loads(line) for line in r.text.split('\n') if line]
+    for record in records:
+        prefix_url = 'https://commoncrawl.s3.amazonaws.com/'
+        data_url = prefix_url + record['filename']
+        start_byte = int(record['offset'])
+        end_byte = start_byte + int(record['length'])
+        headers = {'Range': f'bytes={start_byte}-{end_byte}'}
+        r = requests.get(data_url, headers=headers)
+        data = zlib.decompress(r.content, wbits=zlib.MAX_WBITS | 16)
+        try:
+            soap = bs4.BeautifulSoup(data.decode('utf-8'), 'html.parser')
+            res = re.search('{}'.format(reg_expr), soap.text)
+        except:
+            ans.append('err_in_analyze ' + url)
+            continue
+        if res != None:
+            ans.append(url)
+    return ans
 
 
 def process_sites_wrapper(i):
     index = requests.get('https://index.commoncrawl.org/collinfo.json').json()
     api_url = index[0]['cdx-api']
-    while not queue.empty():
-        url = queue.get()
-        try:
-            record = process_sites(url, api_url)
-        except Exception as e:
+    fio = 'Сергеев Александр Михайлович'
+    fio = fio.split()
+    reg_expr = '({surname})|((({name[0]}|{name}).+)&(({patronymic[0]}|{patronymic}).+))'.format(
+        surname=fio[0], name=fio[1], patronymic=fio[2])
+    with open('data/part_{}.txt'.format(i), 'w') as file:
+        while not queue.empty():
+            url = queue.get()
+            try:
+                record = process_sites(url, api_url, reg_expr)
+            except Exception as e:
+                with lock:
+                    print(url, e, file=sys.stderr)
+                record = 'skip ' + url
+            print(record, sep='\n', file=file)
+
+            # счетчик должен атомарно обновиться
             with lock:
-                print(url, e, file=sys.stderr)
-            record = dict()
-
-        record_str = json.dumps(record, ensure_ascii=False)
-        print(record_str, file=f_json)
-
-        # счетчик должен атомарно обновиться
-        with lock:
-            pbar.update(1)
+                pbar.update(1)
 
 
 if __name__ == '__main__':
