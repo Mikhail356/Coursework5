eb656430d763030ed4271dcf8cb5680cf71345d8
Misha <mikhail9vozdev@yandex.com>
2021-11-04 13:00:25 +0300

    Stupid async in while try except In future may be remake Now it works like this, though for a very long time...

--- fetch.py
+++ fetch.py
@@ -1,7 +1,7 @@
 import sqlite3
 import cdx_toolkit
-from time import time
-import bs4
+from time import sleep
+# import bs4
 from readability import Document
 
 
@@ -16,26 +16,47 @@ def get_data(db: str) -> None:
     while url:
         url = cur.execute(
             """UPDATE queue SET load_started = current_timestamp
-            FROM (SELECT id FROM queue WHERE load_started IS NULL LIMIT 1) FREE
+            FROM (SELECT id FROM queue WHERE load_started IS NULL LIMIT 1) free
             WHERE queue.id = free.id
             RETURNING queue.id, queue.url""").fetchone()
+        con.commit()
         if not url:
             break
         id_ = url[0]
         url = url[1]
-        print(url)  # last update may be useless
-        con.commit()
-        for obj in cdx.iter(url, from_ts='202011', to='2020',
-                            filter=['=status:200']):
+        for ind, obj in enumerate(cdx.iter(url, from_ts='202011', to='2020',
+                                           filter=['=status:200'])):
             doc = Document(obj.content)
-            soup = bs4.BeautifulSoup(doc.summary(), 'html.parser')
-            cur.execute(
-                """INSERT INTO content(url, raw_cont) VALUES (?, ?) 
-                """, (obj['url'], soup.text))
-            con.commit()
-        cur.execute(
-            """UPDATE queue SET load_complete = ? 
-                WHERE id = ?""", (time(), id_))
+            print(ind)
+            # soup = bs4.BeautifulSoup(doc.summary(), 'html.parser')
+            while True:
+                try:
+                    cur.execute(
+                        """INSERT INTO content(url, raw_cont) VALUES (?, ?) 
+                        """, (obj['url'], doc.summary()))
+                    # in place doc.summary() were soup.text
+                    break
+                except:
+                    sleep(0.0001)
+            if ind > 100:
+                con.commit()
+            while True:
+                try:
+                    cur.execute("""UPDATE queue 
+                    SET load_complete = current_timestamp 
+                    WHERE id = ?""", (id_))
+                    break
+                except:
+                    sleep(0.0001)
+            while True:
+                try:
+                    size = cur.execute("""SELECT page_count * page_size as 
+                    size FROM pragma_page_count(), 
+                    pragma_page_size()""").fetchone()
+                    print(size)
+                    break
+                except:
+                    sleep(0.0001)
     con.commit()
     con.close()
     return
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 python3 progress_bar.py &
-for ((i = 1; i <= 15; i++ ))
+for ((i = 1; i <= 8; i++ ))
 do
 python3 fetch.py &
 done
+trap "pkill -P $$" EXIT
 wait
