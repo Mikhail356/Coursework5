62912a85c2955fb1eea9546bf3aef14dd499e929
Misha <mikhail9vozdev@yandex.com>
2021-11-20 00:12:04 +0300

    restruction code, find new errors...

--- fetch.py
+++ fetch.py
@@ -15,59 +15,56 @@ def grouper(iterable, n, fillvalue=None):
 
 def save_batch(batch, id_, db):
     while True:
-        # try:
         with sqlite3.connect(db) as connection:
             cursor = connection.cursor()
             for document in batch:
-                # try:
-                cursor.execute(
-                    """INSERT INTO content(url, raw_cont) 
-                    VALUES (:url, :summary) """, document)
-                # print(id_, type(id_))
-                cursor.execute("""UPDATE queue 
-                    SET load_complete = current_timestamp 
-                    WHERE id = ?""", (id_,))
-                # except Exception as e:
-                # print(document['url'], document['summary'][:10_000],
-                #   sep='\n\n\n')
+                if isinstance(document, dict):
+                    cursor.execute("""
+                        INSERT INTO content(url, raw_cont) 
+                        VALUES (:url, :summary)""", document)
+                    cursor.execute("""
+                        UPDATE queue 
+                        SET load_complete = current_timestamp 
+                        WHERE id = ?""", (id_,))
             connection.commit()
-            size = cursor.execute("""
-            SELECT page_count * page_size as size
-            FROM pragma_page_count(), pragma_page_size()
-            """).fetchone()
-            print(f'{size/1048576} Mbyte')
-            break
-        # except Exception as e:
-        #     print(e, id_)
-        #     sleep(0.0001)
+        break
 
 
 def get_data(db: str) -> None:
-    con = sqlite3.connect(db)
-    cur = con.cursor()
-    url = cur.execute(
-        """SELECT id, url FROM queue 
-           WHERE load_started IS NULL 
-           LIMIT 1""").fetchone()
-    con.close()
+    url = ''
     cdx = cdx_toolkit.CDXFetcher(source='cc')
+    with sqlite3.connect(db) as connection:
+        cursor = connection.cursor()
+        url = cursor.execute(
+            """
+            SELECT id, url FROM queue 
+            WHERE load_started IS NULL 
+            LIMIT 1""").fetchone()
+
     while url:
-        con = sqlite3.connect(db)
-        cur = con.cursor()
-        url = cur.execute(
-            """UPDATE queue SET load_started = current_timestamp
-            FROM (SELECT id FROM queue WHERE load_started IS NULL LIMIT 1) free
-            WHERE queue.id = free.id
-            RETURNING queue.id, queue.url""").fetchone()
-        con.commit()
-        con.close()
+        with sqlite3.connect(db) as connection:
+            cursor = connection.cursor()
+            size = cursor.execute("""
+                SELECT page_count * page_size as size
+                FROM pragma_page_count(), pragma_page_size()
+                """).fetchone()
+            print(f'{size[0]/1048576} Mbyte')
+            if size[0]/1048576 > 5120:
+                break
+            url = cursor.execute("""
+                UPDATE queue SET load_started = current_timestamp
+                FROM 
+                (SELECT id FROM queue WHERE load_started IS NULL LIMIT 1) free
+                WHERE queue.id = free.id
+                RETURNING queue.id, queue.url""").fetchone()
+            connection.commit()
         if not url:
             break
         id_ = url[0]
         url_ = url[1]
         cdx_iterator = cdx.iter(url_, from_ts='202011', to='202012',
                                 filter=['=status:200'])
-        processed = ({"url": doc['url'],
+        processed = ({"url": str(doc['url']),
                       "summary":
                       str(bs4.BeautifulSoup(
                           Document(doc.content).summary(), 'html.parser').text)
@@ -75,9 +72,6 @@ def get_data(db: str) -> None:
                      )
         for batch in grouper(processed, 100):
             save_batch(batch, id_, db)
-    con.commit()
-    con.close()
-    return
 
 
 if __name__ == '__main__':
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 progress_bar.py &
-for ((i = 1; i <= 1; i++ ))
+for ((i = 1; i <= 4; i++ ))
 do
 python3 fetch.py &
 done
