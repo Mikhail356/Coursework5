3ec6dd868ce8973e65ae9bf95236042a4de4c1d4
Misha <mikhail9vozdev@yandex.com>
2022-03-08 22:02:37 +0300

    working solution speed 1.5 [sec/it]

--- sqlsearch.py
+++ sqlsearch.py
@@ -17,32 +17,48 @@ def check_name():
         names = ['"'+(i[0][:-1] if i[0][-1] ==
                       'а' else i[0]) + '"' for i in names]
         cursor = connection.cursor()
-        cursor.execute(
-            "CREATE VIRTUAL TABLE texts USING fts5(id, cont);")
-        cursor.execute(
-            "CREATE VIRTUAL TABLE names USING fts5(id, name, cont_id);")
+        # cursor.execute(
+        #     "CREATE VIRTUAL TABLE texts USING fts5(id, cont);")
+        # cursor.execute(
+        #     "CREATE VIRTUAL TABLE names USING fts5(id, name, cont_id);")
         cursor.execute(
             "attach 'alldb.sqlite3' as db1;")
         cursor.execute(
             "attach 'db-names_sqlite3.sqlite3' as db2;")
         cursor.execute(
             "INSERT INTO texts (id, cont) Select id, raw_cont from db1.content;")
+        # cursor.execute(
+        #     "INSERT INTO names (id, name) Select id, lastname from db2.man;")
         print(cursor.execute(
             "select count(*) from texts;"
         ).fetchall())
+        # print(cursor.execute(
+        #     "select count(*) from names;"
+        # ).fetchall())
         for i in tqdm(range(len(names))):
             texts_id = cursor.execute(
                 "SELECT texts.id  FROM texts  WHERE texts.cont MATCH ?;", (
                     names[i],)
             ).fetchall()
+            for j in range(len(names)):
+                if j != i:
+                    texts_id_j = cursor.execute(
+                        "SELECT texts.id  FROM texts  WHERE texts.cont MATCH ?;", (
+                            names[j],)
+                    ).fetchall()
+                    tmp = list(set(texts_id) & set(texts_id_j))
+            # if(len(tmp) > 0):
+            #     print(names[i], names[j], tmp,
+            #           texts_id, texts_id_j, sep='\n')
+
         #     cursor.execute(
         #         "INSERT INTO names (cont_id) VALUES (?);", (texts_id[0],))
         # with open('res.txt', 'w') as file:
         #     file.write(print(cursor.execute(
         #         "select * from names;"
         #     ).fetchall()))
-        cursor.execute(
-            "DROP TABLE texts;")
+        # cursor.execute(
+        #     "DROP TABLE texts;")
 
 
 check_name()
