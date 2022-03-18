04a9bd2bcdc1060e722aa3dcfb06525c2f4ec2f5
Misha <mikhail9vozdev@yandex.com>
2022-03-18 12:42:14 +0300

    Search nearby lastname

--- sqlsearch.py
+++ sqlsearch.py
@@ -1,21 +1,29 @@
 import sqlite3
+from time import time
 from tqdm import tqdm
 
 DATABASE = 'database.sqlite3'
 
 
-def check_name():
+def base_connection_with_names():
+    """
+    Creates and fills in tables that take into account 
+    the relationship between surnames from the "man" table 
+    and the contents from the "content" table, as well as 
+    between surnames that exist in the same text from "content".
+
+    All of the above is performed in the database DATABASE.
+    """
     names = []
     with sqlite3.connect(DATABASE) as connection:
         cursor = connection.cursor()
+
         names = cursor.execute(
             'select lastname, id from man;'
         ).fetchall()
         names = [('"'+(i[0][:-1] if i[0][-1] ==
                        'а' else i[0]) + '"', i[1]) for i in names]
-        # print(names[:10])
-        # cursor.execute("DROP TABLE texts;")
-        # cursor.execute("DROP TABLE names;")
+
         cursor.execute(
             "CREATE VIRTUAL TABLE IF NOT EXISTS texts USING fts5(id, cont);")
         cursor.execute(
@@ -23,58 +31,43 @@ def check_name():
         if cursor.execute('select count(*) from texts;').fetchone()[0] == 0:
             cursor.execute(
                 "INSERT INTO texts (id, cont) Select id, cont from content;")
-        # cursor.execute(
-        #     "INSERT INTO names (id, lastname) Select id, lastname from man;")
-        # print(cursor.execute(
-        #     "select count(*) from texts;"
-        # ).fetchall())
-        # print(cursor.execute(
-        #     "select count(*) from names;"
-        # ).fetchall())
+
         # Filling nametext matching
         if(cursor.execute("select count(*) from nametext").fetchone()[0] == 0):
             for i in tqdm(range(len(names))):
                 cursor.execute(
-                    "INSERT INTO nametext(cont_id, name_id) SELECT * FROM (SELECT texts.id  FROM texts  WHERE texts.cont MATCH ?) LEFT JOIN (SELECT id from man where id = ?)", (names[i][0], names[i][1]))
-            connection.commit()
-        # print(cursor.execute("select count(*) from nametext").fetchone())
-        for i in tqdm(range(len(names))):
-            texts_id = cursor.execute(
-                "SELECT texts.id  FROM texts  WHERE texts.cont MATCH ?;", (
-                    names[i][0],)
-            ).fetchall()
-            rows = []
-            for j in range(len(names)):
-                if j != i:
-                    texts_id_j = cursor.execute(
-                        "SELECT texts.id  FROM texts  WHERE texts.cont MATCH ?;", (
-                            names[j][0],)
-                    ).fetchall()
-                    texts = list(set(texts_id) & set(texts_id_j))
-                    rows += [(names[i][1], names[j][1], cont_id[0])
-                             for cont_id in texts]
-            cursor.executemany(
-                "INSERT INTO namenamecont(name1, name2, cont_id) VALUES (?, ?, ?)", rows)
-            # for j in range(len(names)):
-            #     if j != i:
-            #         texts_id_j = cursor.execute(
-            #             "SELECT texts.id  FROM texts  WHERE texts.cont MATCH ?;", (
-            #                 names[j][0],)
-            #         ).fetchall()
-            #         tmp = list(set(texts_id) & set(texts_id_j))
-
-            # if(len(tmp) > 0):
-            #     print(names[i], names[j], tmp,
-            #           texts_id, texts_id_j, sep='\n')
-
-            #     cursor.execute(
-            #         "INSERT INTO names (cont_id) VALUES (?);", (texts_id[0],))
-            # with open('res.txt', 'w') as file:
-            #     file.write(print(cursor.execute(
-            #         "select * from names;"
-            #     ).fetchall()))
-            # cursor.execute(
-            #     "DROP TABLE texts;")
+                    "INSERT INTO nametext(cont_id, name_id) SELECT texts.id cont_id, ? name_id FROM texts WHERE texts.cont MATCH ?", (names[i][1], names[i][0]))
+        cursor.execute(
+            "INSERT INTO namenamecont SELECT NULL id, n1.name_id name1, n2.name_id, n1.cont_id cont_id FROM nametext n1 JOIN nametext n2 ON n1.cont_id = n2.cont_id WHERE n1.name_id <> n2.name_id;"
+        )
+        connection.commit()
 
 
-check_name()
+def check_nearby():
+    with sqlite3.connect(DATABASE) as connection:
+        cursor = connection.cursor()
+        texts_id = cursor.execute(
+            "SELECT distinct cont_id from namenamecont"
+        ).fetchall()
+        for text_id in texts_id:
+            print(text_id)
+            names = cursor.execute(
+                "select id, lastname, firstname, middlename, expert from (select name_id from nametext where nametext.cont_id = ?) as name left join man on man.id = name.name_id;",
+                (text_id[0],)).fetchall()
+            print(names)
+            text = cursor.execute(
+                "select cont from content where id = ?",
+                (text_id[0],)
+            ).fetchall()[0]
+            for name in names:
+                print(name)
+                name = name[1][:-1] if name[1][-1] == 'а' else name[1]
+                index = int(text[0].find(name))
+                print(text[0][max(index-50, 0): min(index+50, len(text[0]))])
+            input()
+            input()
+
+
+start_time = time()
+check_nearby()
+print("--- %s seconds ---" % (time() - start_time))
